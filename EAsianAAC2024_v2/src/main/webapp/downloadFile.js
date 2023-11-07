e4109885e43bb8ccc61e457a1d3b0d95c68b30b7
na0young <0na021126@gmail.com>
2023-11-07 21:10:25 +0900

    [FIX] 23.11.07 수정요청사항

--- EAsianAAC2024_v2/src/main/webapp/downloadFile.js
+++ EAsianAAC2024_v2/src/main/webapp/downloadFile.js
@@ -5,7 +5,7 @@
  document.addEventListener("DOMContentLoaded", function () {
     // 클릭 이벤트를 감지하여 파일 다운로드 함수 호출
     document.getElementById("downloadFile").addEventListener("click", function () {
-        downloadDocxFile("[EN] Proposal for Presentation.docx", "파일의 내용을 여기에 입력하세요.");
+        downloadDocxFile("Early-registration application form.docx", "파일의 내용을 여기에 입력하세요.");
     });
 
     // .docx 파일 다운로드 함수 정의
